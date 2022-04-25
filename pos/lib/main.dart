5792a82d1ad567c4cf9088727eac570e8b8ed786
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-04-25 23:28:27 +0700

    changes

--- pos/lib/main.dart
+++ pos/lib/main.dart
@@ -1,14 +1,28 @@
+import 'dart:io';
+
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:pos/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
   const initialSize = Size(1366, 768);
   appWindow.minSize = initialSize;
   appWindow.size = initialSize;
   appWindow.show();
+
+//
+  var dir = Directory.current.path;
+  var fullPath = "$dir\\cache";
+  if (!Directory(fullPath).existsSync()) {
+    Directory(fullPath).createSync(recursive: true);
+  }
+  await GetStorage("setting", fullPath).initStorage;
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,7 +30,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
