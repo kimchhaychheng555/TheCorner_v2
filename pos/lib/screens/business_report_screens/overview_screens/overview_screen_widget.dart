b26d9940bf1b6e9ca22a5c8b673badb51731614f
PHENHCHET JS <73293076+Phenhchet@users.noreply.github.com>
2022-06-21 23:51:01 +0700

    changed

---
+++ pos/lib/screens/business_report_screens/overview_screens/overview_screen_widget.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:pos/widgets/text_widget.dart';
+
+class OverviewScreenWidget extends StatelessWidget {
+  // Set Paramater For Widget
+  final String? title, subtitle, total;
+  final Color? totalColor, textColor, backgroundColor, borderColor, shadowColor;
+  final Icon? icon;
+  final Icon? iconColor;
+  const OverviewScreenWidget({
+    Key? key,
+    this.title,
+    this.totalColor,
+    this.textColor,
+    this.icon,
+    this.iconColor,
+    this.subtitle,
+    this.total,
+    this.backgroundColor,
+    this.borderColor,
+    this.shadowColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+      width: 200,
+      height: 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: borderColor ?? HexColor("#50B402")),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 1,
+            spreadRadius: 1,
+            offset: Offset(3, 3),
+            color: shadowColor ?? HexColor("#50B402").withOpacity(0.2),
+          )
+        ],
+        color: HexColor("#FFFFFF"),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextWidget(
+                      text: title ?? "",
+                      fontSize: 16,
+                      color: HexColor("7D7D7D"),
+                      fontFamily: "Roboto",
+                    ),
+                    TextWidget(
+                      text: subtitle ?? "",
+                      fontSize: 12,
+                      color: HexColor("7D7D7D"),
+                      fontFamily: "Roboto",
+                    ),
+                  ],
+                ),
+                TextWidget(
+                  text: total ?? "",
+                  fontSize: 25,
+                  color: totalColor,
+                  fontFamily: "Roboto",
+                ),
+              ],
+            ),
+            Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: backgroundColor,
+              ),
+              child: icon ??
+                  Icon(
+                    Icons.shopping_cart,
+                  ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
