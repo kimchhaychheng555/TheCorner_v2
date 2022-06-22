3566f2e63b9e4407f319e9552420c3c2723750e4
PHENHCHET JS <73293076+Phenhchet@users.noreply.github.com>
2022-06-22 19:45:50 +0700

    changed

---
+++ pos/lib/controllers/business_controllers/overview_controller/overview_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class OverviewController extends GetxController {
+  var totalSale = 0.obs;
+  var totalOrder = 0.obs;
+  var totalRevenue = (0.0).obs;
+  var totalProfit = (0.0).obs;
+}
