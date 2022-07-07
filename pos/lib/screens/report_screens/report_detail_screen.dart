749e38c2501480981e09cb781fdcb80f11cd8aa0
PHENHCHET JS <73293076+Phenhchet@users.noreply.github.com>
2022-07-07 20:18:35 +0700

    changed

--- pos/lib/screens/report_screens/report_detail_screen.dart
+++ pos/lib/screens/report_screens/report_detail_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:pos/controllers/report_controllers/report_detail_controller.dart';
+import 'package:pos/screens/report_screens/widgets/report_detail_widget.dart';
 import 'package:pos/widgets/loading_overlay_widget.dart';
+import 'package:pos/widgets/text_widget.dart';
 
 class ReportDetailScreen extends GetResponsiveView<dynamic> {
   ReportDetailScreen({Key? key}) : super(key: key);
@@ -22,7 +25,33 @@ class ReportDetailScreen extends GetResponsiveView<dynamic> {
         ),
         body: LoadingOverlayWidget(
           isLoading: _controller.isLoading.value,
-          child: Container(),
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextWidget(
+                      text: "Invoice".tr,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: HexColor("#1E1E1E"),
+                    ),
+                    TextWidget(
+                      text: "july-06-2022".tr,
+                      fontSize: 15,
+                      color: HexColor("#1E1E1E"),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Expanded(
+                child: ReportDetailWidget(),
+              ),
+            ]),
+          ),
         ),
       ),
     );
