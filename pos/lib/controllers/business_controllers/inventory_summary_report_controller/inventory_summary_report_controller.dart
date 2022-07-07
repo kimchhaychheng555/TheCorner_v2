81c7a08f272072d34d7d3bce35046d0927e201f1
PHENHCHET JS <73293076+Phenhchet@users.noreply.github.com>
2022-07-07 23:21:53 +0700

    changed

--- pos/lib/controllers/business_controllers/inventory_summary_report_controller/inventory_summary_report_controller.dart
+++ pos/lib/controllers/business_controllers/inventory_summary_report_controller/inventory_summary_report_controller.dart
@@ -1,11 +1,19 @@
 import 'dart:convert';
+import 'dart:io';
+// import 'dart:html';
 import 'package:darq/darq.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pos/models/others_models/inventory_summary_report_model.dart';
 import 'package:pos/models/stock_inventory_models/stock_inventory_model.dart';
 import 'package:pos/models/stock_transaction_models/stock_transaction_model.dart';
 import 'package:pos/services/api_service.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
+import 'package:universal_html/html.dart' show AnchorElement;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class InventorySummaryReportController extends GetxController {
   var isLoading = false.obs;
@@ -133,7 +141,46 @@ class InventorySummaryReportController extends GetxController {
     return "${"show".tr}: ${((currentPage.value - 1) * pager.value) + 1} - ${(currentPage.value * pager.value)} ${"show".tr} ${totalRecords.value}";
   }
 
-  void onExportPressed() {}
+  void onExportPressed() async {
+    final Workbook workbook = Workbook();
+    final Worksheet sheet = workbook.worksheets[0];
+    sheet.getRangeByName('A1').setText('Name');
+    sheet.getRangeByName('A1').cellStyle.bold = true;
+    sheet.getRangeByName('A2').setText('Amy');
+    sheet.getRangeByName('A3').setText('Jack');
+    sheet.getRangeByName('A4').setText('Tiya');
+    sheet.getRangeByName('A5').setText('Tiya');
+    sheet.getRangeByName('A6').setText('Tiya');
+    sheet.getRangeByName('B1').setText('Age');
+    sheet.getRangeByName('B1').cellStyle.bold = true;
+    sheet.getRangeByName('B2').setNumber(12);
+    sheet.getRangeByName('B3').setNumber(20);
+    sheet.getRangeByName('B4').setNumber(21);
+    sheet.getRangeByName('B5').setNumber(21);
+    sheet.getRangeByName('C1').setText('Year');
+    sheet.getRangeByName('C1').cellStyle.bold = true;
+    sheet.getRangeByName('C2').setNumber(2022);
+    sheet.getRangeByName('C3').setNumber(2022);
+    sheet.getRangeByName('C4').setNumber(2022);
+    sheet.getRangeByName('C5').setNumber(2022);
+    final List<int> bytes = workbook.saveAsStream();
+    workbook.dispose();
+
+    if (kIsWeb) {
+      AnchorElement(
+          href:
+              'data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}')
+        ..setAttribute('download', 'Output.xlsx')
+        ..click();
+    } else {
+      final String part = (await getApplicationSupportDirectory()).path;
+      final String fileName =
+          Platform.isWindows ? '$part\\Output.xlsx' : '$part/Output.xlsx';
+      final File file = File(fileName);
+      await file.writeAsBytes(bytes, flush: true);
+      OpenFile.open(fileName);
+    }
+  }
 
   void onFilterPressed() {}
 }
