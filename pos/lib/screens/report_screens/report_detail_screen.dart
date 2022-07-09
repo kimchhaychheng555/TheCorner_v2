6a2b946d4171d1a506a7916881312cf4363494f2
PHENHCHET JS <73293076+Phenhchet@users.noreply.github.com>
2022-07-09 17:01:22 +0700

    changed

--- pos/lib/screens/report_screens/report_detail_screen.dart
+++ pos/lib/screens/report_screens/report_detail_screen.dart
@@ -24,35 +24,185 @@ class ReportDetailScreen extends GetResponsiveView<dynamic> {
           ),
         ),
         body: LoadingOverlayWidget(
-          isLoading: _controller.isLoading.value,
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextWidget(
-                      text: "Invoice".tr,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: HexColor("#1E1E1E"),
+            isLoading: _controller.isLoading.value,
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                width: 1000,
+                height: 800,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Center(
+                    child: ListView(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const SizedBox(height: 20),
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                const SizedBox(height: 30),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    TextWidget(
+                                      text: "Logo".tr,
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w500,
+                                      color: HexColor("#005FF2"),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 50),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    TextWidget(
+                                      text: "invoice".tr,
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w500,
+                                      color: HexColor("#333440"),
+                                    ),
+                                    TextWidget(
+                                      text: "july-09-2022".tr,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: HexColor("#333440"),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 50),
+                                Container(
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(0.10),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                        flex: 4,
+                                        child: TextWidget(
+                                          text: "task_descriptipn".tr,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: HexColor("#333440"),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: TextWidget(
+                                          text: "rate".tr,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: HexColor("#333440"),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: TextWidget(
+                                          text: "hours".tr,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: HexColor("#333440"),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: TextWidget(
+                                          text: "total".tr,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w500,
+                                          color: HexColor("#333440"),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                                const ReportDetailWidget(
+                                  name: "មានដុត",
+                                  rate: "0.12",
+                                  hours: "2am",
+                                  total: "100\$",
+                                ),
+                                const SizedBox(height: 30),
+                                const ReportDetailWidget(
+                                  name: "មានដុត",
+                                  rate: "0.12",
+                                  hours: "2am",
+                                  total: "100\$",
+                                ),
+                                const SizedBox(height: 30),
+                                const ReportDetailWidget(
+                                  name: "មានដុត",
+                                  rate: "0.12",
+                                  hours: "2am",
+                                  total: "100\$",
+                                ),
+                                const SizedBox(height: 50),
+                                Container(
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(0.10),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 20, right: 50),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Expanded(
+                                          child: TextWidget(
+                                            text: "task_descriptipn".tr,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500,
+                                            color: HexColor("#333440"),
+                                            textAlign: TextAlign.right,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: TextWidget(
+                                            text: "rate".tr,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500,
+                                            color: HexColor("#333440"),
+                                            textAlign: TextAlign.right,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex: 4,
+                                          child: TextWidget(
+                                            text: "hours".tr,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w500,
+                                            color: HexColor("#333440"),
+                                            textAlign: TextAlign.right,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 30),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
                     ),
-                    TextWidget(
-                      text: "july-06-2022".tr,
-                      fontSize: 15,
-                      color: HexColor("#1E1E1E"),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 30),
-              Expanded(
-                child: ReportDetailWidget(),
-              ),
-            ]),
-          ),
-        ),
+            )),
       ),
     );
   }
--- pos/lib/screens/report_screens/widgets/report_detail_widget.dart
+++ pos/lib/screens/report_screens/widgets/report_detail_widget.dart
@@ -1,126 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:pos/constants/constants.dart';
-import 'package:pos/controllers/report_controllers/report_controller.dart';
-import 'package:pos/services/app_service.dart';
-import 'package:pos/widgets/status_widget.dart';
 import 'package:pos/widgets/text_widget.dart';
-import 'package:responsive_table/responsive_table.dart';
 
 class ReportDetailWidget extends StatelessWidget {
-  const ReportDetailWidget({Key? key}) : super(key: key);
+  final String? name;
+  final String? rate;
+  final String? hours;
+  final String? total;
+  const ReportDetailWidget({
+    Key? key,
+    this.name,
+    this.rate,
+    this.hours,
+    this.total,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    ReportController _controller = Get.find();
-    return Obx(
-      () => Card(
-        borderOnForeground: true,
-        elevation: 0,
-        child: Column(
-          children: [
-            Expanded(
-              child: ResponsiveDatatable(
-                reponseScreenSizes: const [ScreenSize.xs],
-                headers: [
-                  DatatableHeader(
-                    text: "invoice_number".tr,
-                    value: "invoice",
-                    show: true,
-                    textAlign: TextAlign.center,
-                    sourceBuilder: (value, row) {
-                      return TextWidget(
-                        text: value,
-                        color: Colors.black,
-                        textAlign: TextAlign.center,
-                      );
-                    },
-                  ),
-                  DatatableHeader(
-                    text: "sub_total".tr,
-                    value: "subtotal",
-                    show: true,
-                    textAlign: TextAlign.center,
-                    sourceBuilder: (value, row) {
-                      return TextWidget(
-                        text: AppService.currencyFormat(value),
-                        fontFamily: "Siemreap",
-                        color: Colors.black,
-                        textAlign: TextAlign.center,
-                      );
-                    },
-                  ),
-                  DatatableHeader(
-                    text: "grand_total".tr,
-                    value: "",
-                    show: true,
-                    textAlign: TextAlign.center,
-                    sourceBuilder: (value, row) {
-                      return TextWidget(
-                        text: AppService.currencyFormat(value),
-                        fontFamily: "Siemreap",
-                        color: Colors.black,
-                        textAlign: TextAlign.center,
-                      );
-                    },
-                  ),
-                  DatatableHeader(
-                    text: "payment_status".tr,
-                    value: "",
-                    show: true,
-                    textAlign: TextAlign.center,
-                    sourceBuilder: (value, row) {
-                      return value
-                          ? StatusWidget(
-                              backgroundColor: successColor,
-                              child: TextWidget(
-                                text: "paid".tr,
-                                color: Colors.white,
-                              ),
-                            )
-                          : StatusWidget(
-                              backgroundColor: warningColor,
-                              child: TextWidget(
-                                text: "unpaid".tr,
-                                color: Colors.white,
-                              ),
-                            );
-                    },
-                  ),
-                ],
-                footers: [
-                  if (_controller.dataSource.isEmpty)
-                    Expanded(
-                      child: Container(
-                        color: HexColor("#ededed"),
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        child: TextWidget(
-                          text: "no_data_available_in_table".tr,
-                          color: textColor,
-                        ),
-                      ),
-                    ),
-                ],
-                headerDecoration: const BoxDecoration(
-                  color: Colors.white,
-                  border: Border(bottom: BorderSide(color: Colors.grey)),
-                ),
-                selectedDecoration: const BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.grey)),
-                  color: Colors.green,
-                ),
-                headerTextStyle: const TextStyle(
-                    color: Colors.black, fontWeight: FontWeight.bold),
-                rowTextStyle: const TextStyle(color: Colors.black),
-                selectedTextStyle: const TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 10),
-          ],
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 4,
+          child: TextWidget(
+            text: name ?? "".tr,
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: HexColor("#333440"),
+            textAlign: TextAlign.center,
+          ),
         ),
-      ),
+        Expanded(
+          child: TextWidget(
+            text: rate ?? "".tr,
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: HexColor("#333440"),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: TextWidget(
+            text: hours ?? "".tr,
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: HexColor("#333440"),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: TextWidget(
+            text: total ?? "".tr,
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: HexColor("#333440"),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
     );
   }
 }
