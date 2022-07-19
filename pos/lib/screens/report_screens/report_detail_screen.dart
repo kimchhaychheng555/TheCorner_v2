bd346c31ba4279fc15b757a92b0b2f447d6c12ae
sokkakada32 <sokkakada32@gmail.com>
2022-07-19 18:50:30 +0700

    changes

--- pos/lib/screens/report_screens/report_detail_screen.dart
+++ pos/lib/screens/report_screens/report_detail_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:pos/controllers/report_controllers/report_detail_controller.dart';
-import 'package:pos/screens/report_screens/widgets/report_detail_widget.dart';
-import 'package:pos/widgets/loading_overlay_widget.dart';
-import 'package:pos/widgets/text_widget.dart';
 
 class ReportDetailScreen extends GetResponsiveView<dynamic> {
   ReportDetailScreen({Key? key}) : super(key: key);
@@ -13,7 +9,6 @@ class ReportDetailScreen extends GetResponsiveView<dynamic> {
 
   @override
   Widget builder() {
-    print("${Get.width}");
     ReportDetailController _controller = Get.find();
     return Scaffold(
       appBar: AppBar(
@@ -24,19 +19,14 @@ class ReportDetailScreen extends GetResponsiveView<dynamic> {
         ),
       ),
       body: Container(
-        constraints: BoxConstraints(maxWidth: 1000),
-        child: Row(
+        constraints: const BoxConstraints(maxWidth: 1000, minWidth: 650),
+        color: Colors.blueAccent,
+        child: Column(
           children: [
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Center(
-                  child: Container(
-                    color: Colors.blue,
-                    width: 650,
-                  ),
-                ),
-              ),
+            Container(
+              width: 600,
+              height: 400,
+              color: Colors.red,
             ),
           ],
         ),
