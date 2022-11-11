ca9a75e5d8f51561b181823cf6ab79b4db9e330e
sokkakada32 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-11-11 15:53:22 +0700

    changes

--- pos/lib/screens/business_report_screens/overview_screens/widgets/overview_screen_widget.dart
+++ pos/lib/screens/business_report_screens/overview_screens/widgets/overview_screen_widget.dart
@@ -27,7 +27,6 @@ class OverviewScreenWidget extends StatelessWidget {
     return Expanded(
         child: Container(
       width: 200,
-      height: 120,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         border: Border.all(color: borderColor ?? HexColor("#50B402")),
@@ -65,6 +64,7 @@ class OverviewScreenWidget extends StatelessWidget {
                     ),
                   ],
                 ),
+                const SizedBox(height: 20),
                 TextWidget(
                   text: total ?? "",
                   fontSize: 25,
